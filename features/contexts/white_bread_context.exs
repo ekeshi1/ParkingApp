2e6c2119b997eeb036ca86a6c5dd04e06ddfe27a
Rajab Azizli <rajabdearly@gmail.com>
2020-11-21 18:53:47 +0200

    started BDD tests on search_parking.feature

--- features/contexts/white_bread_context.exs
+++ features/contexts/white_bread_context.exs
@@ -2,17 +2,40 @@ defmodule WhiteBreadContext do
   use WhiteBread.Context
   use Hound.Helpers
 
+  alias Parking.{Repo, Places.Parking_place, Places.Zone}
+
   feature_starting_state fn  ->
     Application.ensure_all_started(:hound)
     %{}
   end
   scenario_starting_state fn _state ->
     Hound.start_session
+    Ecto.Adapters.SQL.Sandbox.checkout(Parking.Repo)
+    Ecto.Adapters.SQL.Sandbox.mode(Parking.Repo, {:shared, self()})
     %{}
   end
   scenario_finalize fn _status, _state ->
-    #Hound.end_session
+    Ecto.Adapters.SQL.Sandbox.checkin(Parking.Repo)
+    Hound.end_session
   end
 
-  # The skeleton of the steps would be here
+  given_ ~r/^that my email is "(?<email>[^"]+)" and password is "(?<password>[^"]+)"$/,
+  fn state, %{email: email,password: password} ->
+    {:ok, state |> Map.put(:email, email)
+                |> Map.put(:password, password)}
+  end
+
+  and_ ~r/^I am logged in$/, fn state ->
+    #navigate_to "/search"
+    # fill_field({:id, "email"}, state[:email])
+    # fill_field({:id, "password"}, state[:password])
+    # click({:id, "login_button"})
+    #TODO: Log in user
+    {:ok, state}
+  end
+
+  and_ ~r/^the following prices correspond to the zones$/, fn state ->
+    {:ok, state}
+  end
+  # The skleton of the steps would be here
 end
