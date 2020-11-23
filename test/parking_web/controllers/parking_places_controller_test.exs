c15f1ff99eec1472ec876f7520ab54aecb909f5f
Rajab Azizli <rajabdearly@gmail.com>
2020-11-23 23:14:34 +0200

    test for searching modified

--- test/parking_web/controllers/parking_places_controller_test.exs
+++ test/parking_web/controllers/parking_places_controller_test.exs
@@ -4,12 +4,12 @@ defmodule ParkingWeb.ParkingPLacesControllerTest do
   alias Parking.{Repo, Places.Parking_place, Places.Zone}
   alias Parking.Guardian
 
-  @valid_attrs_with_everything %{destination_address: "Pikk 76, 50603, Tartu",hours:"2", minutes:"5" }
+  @valid_attrs_with_everything %{destination_address: "Pikk 76, 50603, Tartu",hours: "2", minutes: "5" }
   @valid_attrs_without_time %{destination_address: "Pikk 76, 50603, Tartu",hours: "", minutes: ""}
 
   setup do
     conn = build_conn()
-      |> Guardian.Plug.sign_in() #Only logged in users can make search, so we make Guadian sign in to be able make search
+      |> Guardian.Plug.sign_out() #Only logged in users can make search, so we make Guadian sign in to be able make search
 
     {:ok, conn: conn}
   end
@@ -19,8 +19,10 @@ defmodule ParkingWeb.ParkingPLacesControllerTest do
 
     test "search with address, hours, and minutes", %{conn: conn} do
 
-      conn = post(conn, Routes.post_path(conn, :create), post: @valid_attrs_with_everything)
-      assert html_response(conn, 200) =~ "Search executed with hour and minutes"
+      conn = post(conn, Routes.parking_place_path(conn, :create), post: @valid_attrs_with_everything)
+      assert redirected_to(conn) == Routes.parking_place_path(conn, :show)
+      conn = get(conn, Routes.parking_place_path(conn, :show))
+      assert html_response(conn, 200) =~ "Here are available parking zones."
 
     end
 
