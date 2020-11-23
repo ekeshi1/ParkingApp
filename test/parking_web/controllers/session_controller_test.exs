bda80df921d70145a89c02b4408bda8abebe3b75
Karl Jääts <kajaat@ttu.ee>
2020-11-23 18:26:49 +0200

    Fixed the login tests

--- test/parking_web/controllers/session_controller_test.exs
+++ test/parking_web/controllers/session_controller_test.exs
@@ -2,28 +2,40 @@ defmodule ParkingWeb.SessionControllerTest do
   use ParkingWeb.ConnCase
 
   alias Parking.Account
+  alias Parking.Guardian
 
-  describe "log in" do
+  setup do
+    conn = build_conn()
+      |> Guardian.Plug.sign_out()
 
-    test "log in succesful", %{conn: conn} do
-      Account.create_user(%{email: "bob@gmail.com", license: "356654", name: "some name", password: "123"})
-      conn = post conn, "/sessions/create", %{session: [email: "bob@gmail.com", password: "123"]}
-      conn = get conn, redirected_to(conn)
-      assert html_response(conn, 200) =~ ~r/Successfully logged in/
-    end
+    {:ok, conn: conn}
+  end
 
-    test "log in unsuccesful (no such user)", %{conn: conn} do
-      conn = post conn, "/sessions/create", %{session: [email: "gary@gmail.com", password: "123"]}
-      conn = get conn, redirected_to(conn)
-      assert html_response(conn, 200) =~ ~r/User doesn't exist/
-    end
 
-    test "log in unsuccesful (wrong password)", %{conn: conn} do
-      Account.create_user(%{email: "a@gmail.com", license: "123213", name: "some name", password: "123"})
-      conn = post conn, "/sessions/create", %{session: [email: "a@gmail.com", password: "999"]}
-      conn = get conn, redirected_to(conn)
-      assert html_response(conn, 200) =~ ~r/Wrong password! Please try again/
-    end
+  test "log in succesful", %{conn: conn} do
+    Account.create_user(%{email: "bob@gmail.com", license: "356654", name: "some name", password: "123"})
+    conn = post conn, "/sessions", %{session: [email: "bob@gmail.com", password: "123"]}
+    assert Parking.Authentication.load_current_user(conn)
+  end
+
+  test "log in unsuccesful (no such user)", %{conn: conn} do
+    conn = post conn, "/sessions", %{session: [email: "gary@gmail.com", password: "123"]}
+    assert !Parking.Authentication.load_current_user(conn)
+  end
+
+  test "log in unsuccesful (wrong password)", %{conn: conn} do
+    Account.create_user(%{email: "a@gmail.com", license: "123213", name: "some name", password: "123"})
+    conn = post conn, "/sessions", %{session: [email: "a@gmail.com", password: "999"]}
+    assert !Parking.Authentication.load_current_user(conn)
+  end
+
+  test "log out", %{conn: conn} do
+    Account.create_user(%{email: "e@gmail.com", license: "3467", name: "some name", password: "123"})
+    conn = post conn, "/sessions", %{session: [email: "e@gmail.com", password: "123"]}
+
+    id = Parking.Authentication.load_current_user(conn).id
+    conn = delete conn, "/sessions/#{id}", Parking.Authentication.load_current_user(conn)
+    assert !Parking.Authentication.load_current_user(conn)
   end
 
 end
