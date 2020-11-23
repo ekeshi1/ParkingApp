3b7d15392c71d0cbbec3d6332068af27ffe5b482
Rajab Azizli <rajabdearly@gmail.com>
2020-11-23 23:39:49 +0200

    search parking place test modified

--- test/parking_web/controllers/parking_places_controller_test.exs
+++ test/parking_web/controllers/parking_places_controller_test.exs
@@ -6,6 +6,8 @@ defmodule ParkingWeb.ParkingPLacesControllerTest do
 
   @valid_attrs_with_everything %{destination_address: "Pikk 76, 50603, Tartu",hours: "2", minutes: "5" }
   @valid_attrs_without_time %{destination_address: "Pikk 76, 50603, Tartu",hours: "", minutes: ""}
+  @valid_attrs_without_minutes%{destination_address: "Pikk 76, 50603, Tartu",hours: "2", minutes: ""}
+  @valid_attrs_without_hours%{destination_address: "Pikk 76, 50603, Tartu",hours: "", minutes: "45"}
 
   setup do
     conn = build_conn()
@@ -27,11 +29,26 @@ defmodule ParkingWeb.ParkingPLacesControllerTest do
     end
 
     test "search with address, without hours and minutes", %{conn: conn} do
-      conn = post(conn, Routes.parking_place_path(conn, :create), post: @valid_attrs_with_everything)
+      conn = post(conn, Routes.parking_place_path(conn, :create), post: @valid_attrs_without_time)
       # assert redirected_to(conn) == Routes.parking_place_path(conn, :create)
       conn = get(conn, Routes.parking_place_path(conn, :create))
       assert html_response(conn, 200) =~ "Here are the available parking zones."
     end
+
+    test "search with address, hours and without minutes", %{conn: conn} do
+      conn = post(conn, Routes.parking_place_path(conn, :create), post: @valid_attrs_with_only_hours)
+      # assert redirected_to(conn) == Routes.parking_place_path(conn, :create)
+      conn = get(conn, Routes.parking_place_path(conn, :create))
+      assert html_response(conn, 200) =~ "Here are the available parking zones with estimated pricing."
+    end
+
+    test "search with address, without hours and minutes", %{conn: conn} do
+      conn = post(conn, Routes.parking_place_path(conn, :create), post: @valid_attrs_without_time)
+      # assert redirected_to(conn) == Routes.parking_place_path(conn, :create)
+      conn = get(conn, Routes.parking_place_path(conn, :create))
+      assert html_response(conn, 200) =~ "Here are the available parking zones with estimated pricing."
+    end
+
   end
 
 end
