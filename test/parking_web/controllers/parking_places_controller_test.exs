509c31e2ade6324511b092476cdaf747988c9a8e
Rajab Azizli <rajabdearly@gmail.com>
2020-11-23 22:48:31 +0200

    parking places controller test modified

--- test/parking_web/controllers/parking_places_controller_test.exs
+++ test/parking_web/controllers/parking_places_controller_test.exs
@@ -4,6 +4,9 @@ defmodule ParkingWeb.ParkingPLacesControllerTest do
   alias Parking.{Repo, Places.Parking_place, Places.Zone}
   alias Parking.Guardian
 
+  @valid_attrs_with_everything %{destination_address: "Pikk 76, 50603, Tartu",hours:"2", minutes:"5" }
+  @valid_attrs_without_time %{destination_address: "Pikk 76, 50603, Tartu",hours: "", minutes: ""}
+
   setup do
     conn = build_conn()
       |> Guardian.Plug.sign_in() #Only logged in users can make search, so we make Guadian sign in to be able make search
@@ -16,8 +19,12 @@ defmodule ParkingWeb.ParkingPLacesControllerTest do
 
     test "search with address, hours, and minutes", %{conn: conn} do
 
-      conn = post conn, "search/some", %{}
+      conn = post(conn, Routes.post_path(conn, :create), post: @valid_attrs_with_everything)
+      assert html_response(conn, 200) =~ "Search executed with hour and minutes"
+
     end
+
+
   end
 
 end
